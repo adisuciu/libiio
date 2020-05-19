825d4c79a4deb552099ff27e5b3d6ec03eda45c8
Adrian Suciu <adrian.suciu1988@gmail.com>
2020-05-19 11:57:07 +0300

    dnssd-windows: Added support for multiple network interfaces
    https://github.com/mjansson/mdns/commit/c26823f26911291afa8e78fa4384bddd3e880374
    added support for multiple network interfaces.
    This commit enables multiple network interfaces for Windows using the
    commit above
    
    Signed-off-by: Adrian Suciu <adrian.suciu1988@gmail.com>

--- dns_sd_windows.c
+++ dns_sd_windows.c
@@ -17,6 +17,47 @@
 #  include <netdb.h>
 #endif
 
+
+static mdns_string_t
+ipv6_address_to_string(char* buffer, size_t capacity, const struct sockaddr_in6* addr,
+	size_t addrlen) {
+	char host[NI_MAXHOST] = { 0 };
+	char service[NI_MAXSERV] = { 0 };
+	int ret = getnameinfo((const struct sockaddr*)addr, addrlen, host, NI_MAXHOST, service,
+		NI_MAXSERV, NI_NUMERICSERV | NI_NUMERICHOST);
+	int len = 0;
+	if (ret == 0) {
+		if (addr->sin6_port != 0)
+			len = snprintf(buffer, capacity, "[%s]:%s", host, service);
+		else
+			len = snprintf(buffer, capacity, "%s", host);
+	}
+	if (len >= (int)capacity)
+		len = (int)capacity - 1;
+	mdns_string_t str = { buffer, len };
+	return str;
+}
+
+static mdns_string_t
+ipv4_address_to_string(char* buffer, size_t capacity, const struct sockaddr_in* addr,
+	size_t addrlen) {
+	char host[NI_MAXHOST] = { 0 };
+	char service[NI_MAXSERV] = { 0 };
+	int ret = getnameinfo((const struct sockaddr*)addr, addrlen, host, NI_MAXHOST, service,
+		NI_MAXSERV, NI_NUMERICSERV | NI_NUMERICHOST);
+	int len = 0;
+	if (ret == 0) {
+		if (addr->sin_port != 0)
+			len = snprintf(buffer, capacity, "%s:%s", host, service);
+		else
+			len = snprintf(buffer, capacity, "%s", host);
+	}
+	if (len >= (int)capacity)
+		len = (int)capacity - 1;
+	mdns_string_t str = { buffer, len };
+	return str;
+}
+
 static int new_discovery_data(struct dns_sd_discovery_data** data)
 {
 	struct dns_sd_discovery_data* d;
@@ -34,6 +75,113 @@ void dnssd_free_discovery_data(struct dns_sd_discovery_data* d)
 	free(d->hostname);
 	free(d);
 }
+
+static int
+open_client_sockets(int* sockets, int max_sockets) {
+	// When sending, each socket can only send to one network interface
+	// Thus we need to open one socket for each interface and address family
+	int num_sockets = 0;
+
+#ifdef _WIN32
+	IP_ADAPTER_ADDRESSES* adapter_address = 0;
+	unsigned int address_size = 8000;
+	unsigned int ret;
+	unsigned int num_retries = 4;
+	do {
+		adapter_address = malloc(address_size);
+		ret = GetAdaptersAddresses(AF_UNSPEC, GAA_FLAG_SKIP_MULTICAST | GAA_FLAG_SKIP_ANYCAST, 0,
+			adapter_address, &address_size);
+		if (ret == ERROR_BUFFER_OVERFLOW) {
+			free(adapter_address);
+			adapter_address = 0;
+		}
+		else {
+			break;
+		}
+	} while (num_retries-- > 0);
+
+	if (!adapter_address || (ret != NO_ERROR)) {
+		free(adapter_address);
+		ERROR("Failed to get network adapter addresses\n");
+		return num_sockets;
+	}
+
+	/*int first_ipv4 = 1;
+	int first_ipv6 = 1;*/
+	for (PIP_ADAPTER_ADDRESSES adapter = adapter_address; adapter; adapter = adapter->Next) {
+		if (adapter->TunnelType == TUNNEL_TYPE_TEREDO)
+			continue;
+		if (adapter->OperStatus != IfOperStatusUp)
+			continue;
+
+		for (IP_ADAPTER_UNICAST_ADDRESS* unicast = adapter->FirstUnicastAddress; unicast;
+			unicast = unicast->Next) {
+			if (unicast->Address.lpSockaddr->sa_family == AF_INET) {
+				struct sockaddr_in* saddr = (struct sockaddr_in*)unicast->Address.lpSockaddr;
+				if ((saddr->sin_addr.S_un.S_un_b.s_b1 != 127) ||
+					(saddr->sin_addr.S_un.S_un_b.s_b2 != 0) ||
+					(saddr->sin_addr.S_un.S_un_b.s_b3 != 0) ||
+					(saddr->sin_addr.S_un.S_un_b.s_b4 != 1)) {
+					/*if (first_ipv4) {
+						service_address_ipv4 = saddr->sin_addr.S_un.S_addr;
+						first_ipv4 = 0;
+					}*/
+					if (num_sockets < max_sockets) {
+						int sock = mdns_socket_open_ipv4(saddr);
+						if (sock >= 0) {
+							char buffer[128];
+							mdns_string_t addr = ipv4_address_to_string(
+								buffer, sizeof(buffer), saddr, sizeof(struct sockaddr_in));
+							DEBUG("Local IPv4 address: %.*s\n", MDNS_STRING_FORMAT(addr));
+							sockets[num_sockets++] = sock;
+						}
+					}
+				}
+			}
+			else if (unicast->Address.lpSockaddr->sa_family == AF_INET6) {
+				struct sockaddr_in6* saddr = (struct sockaddr_in6*)unicast->Address.lpSockaddr;
+				static const unsigned char localhost[] = { 0, 0, 0, 0, 0, 0, 0, 0,
+														  0, 0, 0, 0, 0, 0, 0, 1 };
+				static const unsigned char localhost_mapped[] = { 0, 0, 0,    0,    0,    0, 0, 0,
+																 0, 0, 0xff, 0xff, 0x7f, 0, 0, 1 };
+				if ((unicast->DadState == NldsPreferred) &&
+					memcmp(saddr->sin6_addr.s6_addr, localhost, 16) &&
+					memcmp(saddr->sin6_addr.s6_addr, localhost_mapped, 16)) {
+					/*if (first_ipv6) {
+						memcpy(service_address_ipv6, &saddr->sin6_addr, 16);
+						first_ipv6 = 0;
+					}*/
+					if (num_sockets < max_sockets) {
+						int sock = mdns_socket_open_ipv6(saddr);
+						if (sock >= 0) {
+							char buffer[128];
+							mdns_string_t addr = ipv6_address_to_string(
+								buffer, sizeof(buffer), saddr, sizeof(struct sockaddr_in6));
+							printf("Local IPv6 address: %.*s\n", MDNS_STRING_FORMAT(addr));
+							sockets[num_sockets++] = sock;
+						}
+					}
+				}
+			}
+		}
+	}
+
+	free(adapter_address);
+#endif
+
+	for (int isock = 0; isock < num_sockets; ++isock) {
+#ifdef _WIN32
+		unsigned long param = 1;
+		ioctlsocket(sockets[isock], FIONBIO, &param);
+#else
+		const int flags = fcntl(sockets[isock], F_GETFL, 0);
+		fcntl(sockets[isock], F_SETFL, flags | O_NONBLOCK);
+#endif
+	}
+
+	return num_sockets;
+}
+
 
 static int
 query_callback(int sock, const struct sockaddr* from, size_t addrlen,
@@ -62,9 +210,13 @@ query_callback(int sock, const struct sockaddr* from, size_t addrlen,
 
 	mdns_record_srv_t srv = mdns_record_parse_srv(data, size, offset, length,
 		namebuffer, sizeof(namebuffer));
-	DEBUG("%s : %s SRV %.*s priority %d weight %d port %d\n",
-		addrbuffer, entrytype,
+	DEBUG("%s : SRV %.*s priority %d weight %d port %d\n",
+		addrbuffer,
 		MDNS_STRING_FORMAT(srv.name), srv.priority, srv.weight, srv.port);
+
+	// Go to the last element in the list
+	while (dd->next != NULL)
+		dd = dd->next;
 
 	if (srv.name.length > 1)
 	{
@@ -75,9 +227,7 @@ query_callback(int sock, const struct sockaddr* from, size_t addrlen,
 	strcpy(dd->addr_str, addrbuffer);
 	dd->port = srv.port;
 
-
 	DEBUG("DNS SD: added %s (%s:%d)\n", dd->hostname, dd->addr_str, dd->port);
-
 	// A list entry was filled, prepare new item on the list.
 	if (new_discovery_data(&dd->next)) {
 		ERROR("DNS SD mDNS Resolver : memory failure\n");
@@ -90,12 +240,18 @@ quit:
 int dnssd_find_hosts(struct dns_sd_discovery_data** ddata)
 {
 #ifdef _WIN32
+	const char* hostname = "dummy-host";
 	WORD versionWanted = MAKEWORD(1, 1);
 	WSADATA wsaData;
 	if (WSAStartup(versionWanted, &wsaData)) {
-		ERROR("Failed to initialize WinSock\n");
+		printf("Failed to initialize WinSock\n");
 		return -1;
 	}
+
+	char hostname_buffer[128];
+	DWORD hostname_size = (DWORD)sizeof(hostname_buffer);
+	if (GetComputerNameA(hostname_buffer, &hostname_size))
+		hostname = hostname_buffer;
 #endif
 
 	int ret = 0;
@@ -106,46 +262,57 @@ int dnssd_find_hosts(struct dns_sd_discovery_data** ddata)
 	if (new_discovery_data(&d) < 0) {
 		return -ENOMEM;
 	}
+	*ddata = d;
 
 	size_t capacity = 2048;
 	void* buffer = malloc(capacity);
+	const char service[] = "_iio._tcp.local.";
 	size_t records;
+
 	DEBUG("Sending DNS-SD discovery\n");
 
-	int port = 0;
-	int sock = mdns_socket_open_ipv4(port);
-	if (sock < 0) {
-		ERROR("Failed to open socket: %s\n", strerror(errno));
+	int port = 5353;
+	int sockets[32];
+	int transaction_id[32];
+	int num_sockets = open_client_sockets(sockets, sizeof(sockets) / sizeof(sockets[0]));
+	if (num_sockets <= 0) {
+		ERROR("Failed to open any client sockets\n");
 		return -1;
 	}
-	DEBUG("Opened IPv4 socket for mDNS/DNS-SD\n");
-
-	const char service[] = "_iio._tcp.local.";
+	DEBUG("Opened %d socket%s for mDNS query\n", num_sockets, num_sockets ? "s" : "");
 
 	DEBUG("Sending mDNS query: %s\n", service);
-	if (mdns_query_send(sock, MDNS_RECORDTYPE_PTR,
-		service, strlen(service),
-		buffer, capacity)) {
-		ERROR("Failed to send mDNS query: %s\n", strerror(errno));
-		goto quit;
+	for (int isock = 0; isock < num_sockets; ++isock) {
+		transaction_id[isock] = mdns_query_send(sockets[isock], MDNS_RECORDTYPE_PTR, service, strlen(service), buffer,
+			capacity);
+		if (transaction_id[isock] <= 0)
+			ERROR("Failed to send mDNS query: %s\n", strerror(errno));
 	}
 
-	DEBUG("Reading mDNS replies\n");
-	for (int i = 0; i < 5; ++i) {
+	// This is a simple implementation that loops for 10 seconds or as long as we get replies
+	// A real world implementation would probably use select, poll or similar syscall to wait
+	// until data is available on a socket and then read it
+	DEBUG("Reading mDNS query replies\n");
+	for (int i = 0; i < 10; ++i) {
+		size_t records;
 		do {
-			records = mdns_query_recv(sock, buffer, capacity, query_callback, d, 1);
+			records = 0;
+			for (int isock = 0; isock < num_sockets; ++isock) {
+				if (transaction_id[isock] > 0)
+				records +=
+					mdns_query_recv(sockets[isock], buffer, capacity, query_callback, d, transaction_id[isock]);
+			}
 		} while (records);
 		if (records)
 			i = 0;
 		Sleep(100);
 	}
 
-	*ddata = d;
 quit:
 	free(buffer);
-
-	mdns_socket_close(sock);
-	DEBUG("Closed socket\n");
+	for (int isock = 0; isock < num_sockets; ++isock)
+		mdns_socket_close(sockets[isock]);
+	DEBUG("Closed socket%s\n", num_sockets ? "s" : "");
 
 #ifdef _WIN32
 	WSACleanup();
